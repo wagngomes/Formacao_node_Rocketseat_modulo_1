a7df1162c6b8fca7dc7d260cd724deeb6071b137
wagngomes <wagner.gomes.cx2@gmail.com>
2024-08-20 19:58:29 +0000

    recebendo Json do front e exibindo

--- src/server.js
+++ src/server.js
@@ -2,9 +2,26 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const { method, url } = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try{
+
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+
+    }catch{
+        
+        req.body = null
+    }
+
+
 
     if (method === 'GET' && url === '/users'){
         return res
@@ -14,10 +31,12 @@ const server = http.createServer((req, res) => {
 
     if (method === 'POST' && url === '/users'){
 
+        const { nome, email } = req.body
+
         users.push({
             id: 1,
-            nome: 'john doe',
-            email: 'johmdoe@email.com'
+            nome,
+            email,
         })
         return res.writeHead(201).end()
     }
